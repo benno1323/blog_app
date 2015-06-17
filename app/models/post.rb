0759c58642a71767632cbb248d9bf317d8620570
Benito Miranda <benomir82@gmail.com>
2015-06-16 22:26:05 -0500

    Add votable

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
-	has_many :votes
+	has_many :votes, as: :votable
 	has_many :users_who_voted, through: :votes, source: :user # o puede ser has_many :users
 	has_many :comments
 
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   validates :user_id, uniqueness: { scope: :post } # user id es unico por cada post
+  belongs_to :votable, polymorphic: :true
 end
